b18d26d67a194c84ea83cf0e250b53afc32881fe
Alyssa-Ava <liuhongyun2013@sina.com>
2019-05-16 20:03:48 +0800

    city

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="city-header">
+    <div class="iconfont back-icon">&#xe624;</div>
+    城市选择
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .city-header
+    height $headerHeight
+    line-height $headerHeight
+    overflow hidden
+    position relative
+    font-size .32rem
+    text-align center
+    background $bgColor
+    color #fff
+    .back-icon
+      position absolute
+      top 0
+      left 0
+      font-size .32rem
+      color #ffffff
+</style>
