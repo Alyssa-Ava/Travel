0488ebf528950283f5a2dc4493910ff4e9153f1f
Alyssa-Ava <liuhongyun2013@sina.com>
2019-05-26 16:00:57 +0800

    detail-list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,6 +2,7 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
+    <detail-list :list="list"></detail-list>
     <div class="content"></div>
   </div>
 </template>
@@ -9,11 +10,34 @@
 <script>
 import detailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     detailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '三馆',
+          children: [{
+            title: ' 1店'
+          }]
+        }, {
+          title: '五馆'
+        }]
+      }, {
+        title: '学生票'
+      }, {
+        title: '儿童票'
+      }, {
+        title: '特惠票'
+      }]
+    }
   }
 }
 </script>
